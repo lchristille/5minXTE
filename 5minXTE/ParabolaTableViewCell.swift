639b8b465ff45eb64fce2bdfb2ee54a58899323a
felim.black@gmail.com <felim.black@gmail.com>
2016-04-06 21:53:59 +0200

    Initial Commit

---
+++ 5minXTE/ParabolaTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ParabolaTableViewCell.swift
+//  DiocesiAL
+//
+//  Created by Luca Christille on 27/03/16.
+//  Copyright © 2016 Luca Christille. All rights reserved.
+//
+
+import UIKit
+
+class ParabolaTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
