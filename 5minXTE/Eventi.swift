79a1316f599ae3b493247dc6d7f1c00a13f028c6
felim.black@gmail.com <felim.black@gmail.com>
2016-04-07 22:52:18 +0200

    Added Launch files

---
+++ 5minXTE/Eventi.swift
@@ -0,0 +1,14 @@
+//
+//  Eventi.swift
+//  5minXTE
+//
+//  Created by Luca Christille on 06/04/16.
+//  Copyright © 2016 Luca Christille. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+
+
