508bf686419bbb5fccd1a0069499baeef0c2a6ab
felim.black@gmail.com <felim.black@gmail.com>
2016-04-18 21:36:16 +0200

    commit pre branch-swift

--- 5minXTE/EventiViewController.swift
+++ 5minXTE/EventiViewController.swift
@@ -12,8 +12,6 @@ class EventiViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
